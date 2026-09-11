c2cd97432d23f7e5fcddb6fab16a0aee4b5011bc
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-09-11 15:51:08 +0000

    Training

---
+++ Trainmanager.py
@@ -0,0 +1,10 @@
+import time
+import random
+
+# money,produced resources,resources stockplie, prices of resources,wanted resources
+loc1 = [10,["Tools"],[],[],[]]
+loc2 = [10,["Food"],[],[],[]]
+loc3 = [10,["Timber"],[],[],[]]
+#resources pricing timber :2 Food 3 Tools 4
+def resource_gain(Location):
+    pass
